722f1e3c1c17ae86953b2b92850004a4710c8d53
mao-118 <>
2022-06-06 08:20:34 +0000

    Deploying to gh-pages from @ mao-118/fast-vue-admin@da9de03f17a731ec7c6fc57d4480655ae9b96ea6 🚀

--- assets/index.06d7c1fb.js
+++
@@ -1,1 +0,0 @@
-import{b as a,d as s,o as l,f as u,g as c,A as _}from"./index.a6e8dc21.js";const r={class:"app-container"},p=_("h1",null,"Menu1-1",-1),d={name:"menu1-1"},V=Object.assign(d,{setup(m){const e=a("");return(i,t)=>{const o=s("el-input");return l(),u("div",r,[p,c(o,{modelValue:e.value,"onUpdate:modelValue":t[0]||(t[0]=n=>e.value=n)},null,8,["modelValue"])])}}});export{V as default};
