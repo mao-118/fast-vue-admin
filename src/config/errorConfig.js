d51cf45c809960e534f4ebcd6f02abcadd769c5a
mao-118 <383397258@qq.com>
2022-07-20 15:57:09 +0800

    to打印

--- src/config/errorConfig.js
+++ src/config/errorConfig.js
@@ -2,6 +2,7 @@ import { mainStore } from '@/store';
 export function errorHandler(error, instance, info) {
   console.error(error, info);
   console.dir(instance);
+  console.log('出错捕获到了吗？？？？');
   mainStore.addErrorList({
     error,
     instance,
