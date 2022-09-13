81c5ad0aec27fca81a8a82fe067a772e2c80961c
mao-118 <383397258@qq.com>
2022-09-13 15:24:48 +0800

    添加高德加密

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import usePlugin from './plugins';
 import { useProdMock } from '@/utils/mockProdServer';
 // 生产环境下使用mock数据，如不需要则去掉
 useProdMock();
+window._AMapSecurityConfig = {
+  securityJsCode: 'e620277d4218a554566dfff84e7eee6e'
+};
+console.log(window._AMapSecurityConfig, 'config');
 const app = createApp(App);
 usePlugin(app);
 app.use(router);
--- src/views/clipboard/index.vue
+++ src/views/clipboard/index.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="app-container">
     <el-tabs v-model="activeName" class="demo-tabs">
-      <el-tab-pane label="直接复制" name="directly"></el-tab-pane>
-      <el-tab-pane label="指令复制" name="directive"></el-tab-pane>
+      <el-tab-pane label="直接复制" name="directly" />
+      <el-tab-pane label="指令复制" name="directive" />
     </el-tabs>
     <el-form :inline="true" :model="form" class="demo-form-inline">
       <el-form-item>
-        <el-input style="width:300px;" v-model="form.text" placeholder="请输入内容" />
+        <el-input v-model="form.text" style="width:300px;" placeholder="请输入内容" />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="handleCopy" v-if="activeName === 'directly'">Copy</el-button>
-        <el-button type="primary" v-clipboard="form.text" v-else>Copy</el-button>
+        <el-button v-if="activeName === 'directly'" type="primary" @click="handleCopy">Copy</el-button>
+        <el-button v-else v-clipboard="form.text" type="primary">Copy</el-button>
       </el-form-item>
     </el-form>
-
   </div>
 </template>
 <script>
