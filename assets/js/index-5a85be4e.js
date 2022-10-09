58424c18cbcc79d353e70b3c1cd387f661fae0ca
mao-118 <>
2022-10-09 03:30:14 +0000

    Deploying to gh-pages from @ mao-118/fast-vue-admin@491394b0e2c447ecca0322ca5f73c5925e3b3b58 🚀

---
+++ assets/js/index-5a85be4e.js
@@ -0,0 +1,1 @@
+import{_ as a,c as s,i as e,p as n,e as c,b as d,j as r}from"./index-28099bfc.js";const t={},i={class:"app-container"},o=[(a=>(n("data-v-f02c8b66"),a=a(),c(),a))((()=>d("div",{class:"menu"},[r(" menu1 "),d("div",null,"menu1-1")],-1)))];var u=a(t,[["render",function(a,n){return e(),s("div",i,o)}],["__scopeId","data-v-f02c8b66"]]);export{u as default};
