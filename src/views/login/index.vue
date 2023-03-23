1d1fc64be18d43cbf5e330b2feedd227531a5fee
mao-118 <383397258@qq.com>
2023-03-23 17:43:01 +0800

    修改登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,109 +1,43 @@
 <template>
-  <div id="login-main">
-    <div class="main">
-      <div id="a-container" class="container a-container">
-        <form id="a-form" class="form" method="" action="">
-          <h2 class="form_title title">Create Account</h2>
-          <span class="form__span">or use email for registration</span>
-          <input class="form__input" type="text" placeholder="Name" />
-          <input class="form__input" type="text" placeholder="Email" />
-          <input class="form__input" autocomplete="off" type="password" placeholder="Password" />
-          <button class="form__button button submit" @click="register">注 册</button>
-        </form>
-      </div>
-      <div id="b-container" class="container b-container">
-        <form id="b-form" class="form" method="" action="">
-          <h2 class="form_title title">Sign in to Website</h2>
-          <span class="form__span">Please login with your account password</span>
-          <input v-model="username" class="form__input" type="text" placeholder="Username" />
-          <input v-model="password" autocomplete="off" class="form__input" type="password" placeholder="Password" />
-          <a class="form__link cursor-pointer" @click="forgotPwd">Forgot your password?</a>
-          <button v-loading class="form__button button submit" @click="handleLogin">登 录</button>
-        </form>
-      </div>
-      <div id="switch-cnt" class="switch">
-        <div class="switch__circle" />
-        <div class="switch__circle switch__circle--t" />
-        <div id="switch-c1" class="switch__container">
-          <h2 class="switch__title title">欢迎回来 !</h2>
-          <p class="switch__description description">请使用您账号密码登录</p>
-          <button class="switch__button button switch-btn">登 录</button>
-        </div>
-        <div id="switch-c2" class="switch__container is-hidden">
-          <h2 class="switch__title title">Hello Friend !</h2>
-          <p class="switch__description description">Enter your personal details and start journey with us</p>
-          <button class="switch__button button switch-btn">注 册</button>
-        </div>
-      </div>
+  <div class="form-content">
+    <div class="form-wrap">
+      <el-form label-width="90px" ref="formEl" :model="form" :rules="rules">
+        <h1 class="mb-6 pt-12">后台管理系统</h1>
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="form.username" placeholder="用户名随便输"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password" placeholder="密码随便输"></el-input>
+        </el-form-item>
+        <el-button class="submit-btn" type="primary" @click="handleLogin"> 登录 </el-button>
+      </el-form>
     </div>
   </div>
 </template>
+
 <script setup>
-import { ElMessage, ElLoading } from 'element-plus'
+import { ElMessage } from 'element-plus'
 import { mainStore } from '@/store'
-let switchCtn = null
-let switchC1 = null
-let switchC2 = null
-let switchCircle = null
-let switchBtn = null
-let aContainer = null
-let bContainer = null
-let allButtons = null
-onMounted(() => {
-  switchCtn = document.querySelector('#switch-cnt')
-  switchC1 = document.querySelector('#switch-c1')
-  switchC2 = document.querySelector('#switch-c2')
-  switchCircle = document.querySelectorAll('.switch__circle')
-  switchBtn = document.querySelectorAll('.switch-btn')
-  aContainer = document.querySelector('#a-container')
-  bContainer = document.querySelector('#b-container')
-  allButtons = document.querySelectorAll('.submit')
-  mainF()
-  changeForm()
+const form  = reactive({
+  username:'',
+  password:''
 })
-
-let getButtons = (e) => e.preventDefault()
-
-let changeForm = (e) => {
-  console.log(111)
-  switchCtn.classList.add('is-gx')
-  setTimeout(function () {
-    switchCtn.classList.remove('is-gx')
-  }, 1500)
-  switchCtn.classList.toggle('is-txr')
-  switchCircle[0].classList.toggle('is-txr')
-  switchCircle[1].classList.toggle('is-txr')
-  switchC1.classList.toggle('is-hidden')
-  switchC2.classList.toggle('is-hidden')
-  aContainer.classList.toggle('is-txl')
-  bContainer.classList.toggle('is-txl')
-  bContainer.classList.toggle('is-z200')
-}
-let mainF = (e) => {
-  for (let i = 0; i < allButtons.length; i++) allButtons[i].addEventListener('click', getButtons)
-  for (let i = 0; i < switchBtn.length; i++) switchBtn[i].addEventListener('click', changeForm)
-}
-const register = () => {
-  ElMessage.warning('暂未开放注册功能')
-}
-const forgotPwd = () => {
-  ElMessage.warning('请联系管理员更改密码')
-}
-const username = ref('admin')
-const password = ref('123456')
-const router = useRouter()
 const route = useRoute()
+const router = useRouter()
+const rules = reactive({
+  username:[{required:true,trigger:'blur',message:'请输入用户名'}],
+  password:[{required:true,trigger:'blur',message:'请输入密码'}],
+})
+const formEl = ref()
 const handleLogin = () => {
-  if (!username.value) return ElMessage.error('请输入账号')
-  if (!password.value) return ElMessage.error('请输入密码')
-  const loading = ElLoading.service({
+  formEl.value.validate((valid, fields) => {
+    if (valid) {
+     const loading = ElLoading.service({
     lock: true,
     text: 'Loading',
     background: 'rgba(0, 0, 0, 0.7)',
   })
-
   setTimeout(() => {
-    if (username.value === 'admin' && password.value === '123456') {
       mainStore.setToken('hejsljlsjfisjdfijsjsjfjdjjasjfksjjsdjfsjisjjdjf')
       const query = route.query
       let redirect = ''
@@ -112,12 +46,17 @@ const handleLogin = () => {
         redirect = query.redirect
         otherQuery = getOtherQuery(query)
       }
-      router.replace({ path: redirect || '/', query: otherQuery })
-    } else {
-      ElMessage.error('用户名或密码错误')
-    }
+      router.replace({ path: redirect || '/', query: otherQuery }).then(()=>{
+        ElMessage.success('登录成功')
+      })
     loading.close()
+
   }, 500)
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+
 }
 const getOtherQuery = (query) => {
   return Object.keys(query).reduce((acc, cur) => {
@@ -128,289 +67,46 @@ const getOtherQuery = (query) => {
   }, {})
 }
 </script>
+
 <style lang="scss" scoped>
-$bg: #edf2f0;
-
-$neu-1: #ecf0f3;
-$neu-2: #d1d9e6;
-
-$white: #f9f9f9;
-$gray: #a0a5a8;
-$black: #181818;
-
-$purple: #4b70e2;
-
-$transition: 1s;
-
-/* Generic */
-#login-main {
+.form-content {
   width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-family: 'Montserrat', sans-serif;
-  font-size: 12px;
-  background-color: $neu-1;
-  color: $gray;
-  overflow: hidden;
-}
-
-/**/
-.main {
+  height: 100%;
+  background: #291a5e;
   position: relative;
-  width: 1000px;
-  min-width: 1000px;
-  min-height: 600px;
-  height: 600px;
-  padding: 25px;
-  background-color: $neu-1;
-  box-shadow: 10px 10px 10px $neu-2, -10px -10px 10px $white;
-  border-radius: 12px;
-  overflow: hidden;
 
-  @media (max-width: 1200px) {
-    transform: scale(0.7);
-  }
-
-  @media (max-width: 1000px) {
-    transform: scale(0.6);
-  }
-
-  @media (max-width: 800px) {
-    transform: scale(0.5);
-  }
-
-  @media (max-width: 600px) {
-    transform: scale(0.4);
-  }
-}
-
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-
-  top: 0;
-  width: 600px;
-  height: 100%;
-  padding: 25px;
-
-  background-color: $neu-1;
-  transition: $transition;
-}
-
-.form {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-
-  &__icon {
-    object-fit: contain;
-    width: 30px;
-    margin: 0 5px;
-    opacity: 0.5;
-    transition: 0.15s;
-
-    &:hover {
-      opacity: 1;
-      transition: 0.15s;
-      cursor: pointer;
-    }
-  }
-
-  &__input {
-    width: 350px;
-    height: 40px;
-    margin: 4px 0;
-
-    padding-left: 25px;
-    font-size: 13px;
-    letter-spacing: 0.15px;
-    border: none;
-    outline: none;
-
-    font-family: 'Montserrat', sans-serif;
-    background-color: $neu-1;
-    transition: 0.25s ease;
-    border-radius: 8px;
-
-    box-shadow: inset 2px 2px 4px $neu-2, inset -2px -2px 4px $white;
-
-    &:focus {
-      box-shadow: inset 4px 4px 4px $neu-2, inset -4px -4px 4px $white;
-    }
-  }
-
-  &__span {
-    margin-top: 30px;
-    margin-bottom: 12px;
-  }
-
-  &__link {
-    color: $black;
-    font-size: 15px;
-    margin-top: 25px;
-    border-bottom: 1px solid $gray;
-    line-height: 2;
-  }
-}
-
-.title {
-  font-size: 34px;
-  font-weight: 700;
-  line-height: 3;
-  color: $black;
-}
-
-.description {
-  font-size: 14px;
-  letter-spacing: 0.25px;
-  text-align: center;
-  line-height: 1.6;
-}
-
-.button {
-  width: 180px;
-  height: 50px;
-  border-radius: 25px;
-  margin-top: 50px;
-  font-weight: 700;
-  font-size: 14px;
-  letter-spacing: 1.15px;
-
-  background-color: $purple;
-  color: $white;
-  box-shadow: 8px 8px 16px $neu-2, -8px -8px 16px $white;
-
-  border: none;
-  outline: none;
-}
-
-/**/
-
-.a-container {
-  z-index: 100;
-  left: calc(100% - 600px);
-}
-
-.b-container {
-  left: calc(100% - 600px);
-  z-index: 0;
-}
-
-.switch {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 400px;
-
-  padding: 50px;
-  z-index: 200;
-  transition: $transition;
-
-  background-color: $neu-1;
-  overflow: hidden;
-
-  box-shadow: 4px 4px 10px $neu-2, -4px -4px 10px $white;
-
-  &__circle {
+  .form-wrap {
+    width: 370px;
+    height: 300px;
     position: absolute;
-    width: 500px;
-    height: 500px;
-    border-radius: 50%;
-    background-color: $neu-1;
-    box-shadow: inset 8px 8px 12px $neu-2, inset -8px -8px 12px $white;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 10px;
+    background: linear-gradient(to bottom, rgb(164, 60, 246), rgb(0, 184, 249));
+    padding: 2px;
 
-    bottom: -60%;
-    left: -60%;
-    transition: $transition;
+    :deep(.el-form) {
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(to bottom, rgb(30, 22, 75), rgb(43, 26, 97));
+      border-radius: 10px;
 
-    &--t {
-      top: -30%;
-      left: 60%;
-      width: 300px;
-      height: 300px;
+      h1 {
+        font-size: 24px;
+        text-align: center;
+        color: #fff;
+      }
+
+      .submit-btn {
+        width: 250px;
+        margin-left: 90px;
+      }
+
+      .el-input {
+        width: 250px;
+      }
     }
-  }
-
-  &__container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    position: absolute;
-    width: 400px;
-    padding: 50px 55px;
-
-    transition: $transition;
-  }
-
-  &__button {
-    cursor: pointer;
-
-    &:hover {
-      box-shadow: 6px 6px 10px $neu-2, -6px -6px 10px $white;
-      transform: scale(0.985);
-      transition: 0.25s;
-    }
-
-    &:active,
-    &:focus {
-      box-shadow: 2px 2px 6px $neu-2, -2px -2px 6px $white;
-      transform: scale(0.97);
-      transition: 0.25s;
-    }
-  }
-}
-
-/**/
-.is-txr {
-  left: calc(100% - 400px);
-  transition: $transition;
-  transform-origin: left;
-}
-
-.is-txl {
-  left: 0;
-  transition: $transition;
-  transform-origin: right;
-}
-
-.is-z200 {
-  z-index: 200;
-  transition: $transition;
-}
-
-.is-hidden {
-  visibility: hidden;
-  opacity: 0;
-  position: absolute;
-  transition: $transition;
-}
-
-.is-gx {
-  animation: is-gx $transition;
-}
-
-@keyframes is-gx {
-  0%,
-  10%,
-  100% {
-    width: 400px;
-  }
-
-  30%,
-  50% {
-    width: 500px;
   }
 }
 </style>
