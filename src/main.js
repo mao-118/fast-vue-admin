1ad3240c652be2e9dbbc1263d11e9a3fd0980719
mao-118 <383397258@qq.com>
2023-04-24 18:08:49 +0800

    feat：message

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './styles/index.scss'
+import 'element-plus/es/components/message/style/css';
 import myPlugin from './plugins'
 import { useProdMock } from '@/utils/mockProdServer'
 // 生产环境下使用mock数据，如不需要则去掉
