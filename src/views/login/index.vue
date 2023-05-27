bb11c8e363d7a3eb7e3991a3f7b5cef748b81719
mao-118 <383397258@qq.com>
2023-05-27 20:00:59 +0800

    登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,90 @@
 <template>
-  <div class="form-content">
-    <div class="form-wrap">
-      <el-form label-width="90px" ref="formEl" :model="form" :rules="rules">
-        <h1 class="mb-6 pt-12">后台管理系统</h1>
-        <el-form-item label="用户名" prop="username">
-          <el-input v-model="form.username" placeholder="用户名随便输"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input v-model="form.password" placeholder="密码随便输"></el-input>
-        </el-form-item>
-        <el-button class="submit-btn" type="primary" @click="handleLogin"> 登录 </el-button>
-      </el-form>
+  <div class="login-container">
+    <div class="login-content">
+      <div class="header flex justify-center items-center">
+        <img src="https://preview.pro.ant.design/logo.svg" alt="logo" class="logo" />
+        <span class="ml-4">登陆</span>
+      </div>
+      <div class="tips text-center mt-2">中小型后台解决方案</div>
+      <div class="login-box mt-6">
+        <el-tabs v-model="activeKey" centered>
+          <el-tab-pane label="账号密码登陆" :name="1"></el-tab-pane>
+          <el-tab-pane label="手机号登陆" :name="2"></el-tab-pane>
+        </el-tabs>
+        <el-form ref="ruleForm" :model="formState" :rules="rules">
+          <template v-if="activeKey === 1">
+            <el-form-item prop="username">
+              <el-input v-model="formState.username" :prefix-icon="User" placeholder="用户名: admin or user">
+              </el-input>
+            </el-form-item>
+
+            <el-form-item prop="password">
+              <el-input
+                v-model="formState.password"
+                :prefix-icon="Lock"
+                type="password"
+                placeholder="密码: 123456"
+                show-password
+                @keyup.enter="onSubmit"
+              />
+            </el-form-item>
+          </template>
+          <template v-else>
+            <el-form-item prop="mobile">
+              <el-input v-model="formState.mobile" :prefix-icon="Iphone" placeholder="请输入手机号!"> </el-input>
+            </el-form-item>
+            <el-form-item prop="msgCode">
+              <div class="flex justify-between items-center">
+                <el-input
+                  v-model="formState.msgCode"
+                  :prefix-icon="Lock"
+                  placeholder="请输入验证码!"
+                  @keyup.enter="onSubmit"
+                >
+                </el-input>
+                <el-button
+                  class="ml-2"
+                  style="height: 40px"
+                  :loading="msgLoading"
+                  :disabled="time !== 60"
+                  @click="sendMsg"
+                >
+                  {{ time === 60 ? `获取验证码` : `${time}s后重新获取` }}</el-button
+                >
+              </div>
+            </el-form-item>
+          </template>
+
+          <el-form-item>
+            <div class="w-full flex justify-between items-center">
+              <el-checkbox v-model="formState.remember">自动登陆</el-checkbox>
+              <a class="login-form-forgot" href="javascript:void(0)">忘记密码?</a>
+            </div>
+          </el-form-item>
+
+          <el-form-item>
+            <el-button :loading="loading" class="w-full" style="height: 40px" type="primary" @click="onSubmit"
+              >登陆</el-button
+            >
+          </el-form-item>
+          <!-- <el-form-item>
+            <div class="flex items-center">
+              <span class="tips mr-2">其他登陆方式: </span>
+              <div class="icon-list flex">
+                <svg-icon
+                  v-for="item in iconList"
+                  :key="item.name"
+                  class="cursor-pointer mr-2"
+                  :name="item.name"
+                  :color="item.enter ? '#1890ff' : ''"
+                  @mouseenter="item.enter = true"
+                  @mouseleave="item.enter = false"
+                />
+              </div>
+            </div>
+          </el-form-item> -->
+        </el-form>
+      </div>
     </div>
   </div>
 </template>
@@ -18,45 +92,49 @@
 <script setup>
 import { ElMessage } from 'element-plus'
 import { mainStore } from '@/store'
-const form  = reactive({
-  username:'',
-  password:''
+import { Lock, User, Iphone } from '@element-plus/icons-vue'
+
+const formState = reactive({
+  username: '',
+  password: '',
+  mobile: '',
+  msgCode: '',
+  remember: true,
 })
 const route = useRoute()
 const router = useRouter()
 const rules = reactive({
-  username:[{required:true,trigger:'blur',message:'请输入用户名'}],
-  password:[{required:true,trigger:'blur',message:'请输入密码'}],
+  username: [{ required: true, trigger: 'blur', message: '请输入用户名!' }],
+  password: [{ required: true, trigger: 'blur', message: '请输入密码!' }],
+  mobile: [{ required: true, trigger: 'blur', message: '请输入手机号!' }],
+  msgCode: [{ required: true, trigger: 'blur', message: '请输入验证码!' }],
 })
-const formEl = ref()
-const handleLogin = () => {
-  formEl.value.validate((valid, fields) => {
+const loading = ref(false)
+const activeKey = ref(1)
+const ruleForm = ref()
+const onSubmit = () => {
+  if (!ruleForm.value) return
+  ruleForm.value.validate((valid, fields) => {
     if (valid) {
-     const loading = ElLoading.service({
-    lock: true,
-    text: 'Loading',
-    background: 'rgba(0, 0, 0, 0.7)',
-  })
-  setTimeout(() => {
-      mainStore.setToken('hejsljlsjfisjdfijsjsjfjdjjasjfksjjsdjfsjisjjdjf')
-      const query = route.query
-      let redirect = ''
-      let otherQuery = {}
-      if (query) {
-        redirect = query.redirect
-        otherQuery = getOtherQuery(query)
-      }
-      router.replace({ path: redirect || '/', query: otherQuery }).then(()=>{
-        ElMessage.success('登录成功')
-      })
-    loading.close()
-
-  }, 500)
+      loading.value = true
+      setTimeout(() => {
+        mainStore.setToken('hejsljlsjfisjdfijsjsjfjdjjasjfksjjsdjfsjisjjdjf')
+        const query = route.query
+        let redirect = ''
+        let otherQuery = {}
+        if (query) {
+          redirect = query.redirect
+          otherQuery = getOtherQuery(query)
+        }
+        router.replace({ path: redirect || '/', query: otherQuery }).then(() => {
+          ElMessage.success('登录成功')
+        })
+        loading.value = false
+      }, 500)
     } else {
       console.log('error submit!', fields)
     }
   })
-
 }
 const getOtherQuery = (query) => {
   return Object.keys(query).reduce((acc, cur) => {
@@ -66,47 +144,58 @@ const getOtherQuery = (query) => {
     return acc
   }, {})
 }
+const msgLoading = ref(false)
+const time = ref(60)
+const timer = ref(0)
+const sendMsg = () => {
+  if (!formState.mobile) return ElMessage.warning('请先输入手机号')
+  msgLoading.value = true
+  setTimeout(() => {
+    ElMessage.success('短信已发送至手机,请注意查收!')
+    msgLoading.value = false
+    time.value--
+    timer.value = window.setInterval(() => {
+      time.value--
+      if (time.value <= 0) {
+        clearInterval(timer.value)
+        time.value = 60
+      }
+    }, 1000)
+  }, 1000)
+}
 </script>
 
 <style lang="scss" scoped>
-.form-content {
+.login-container {
   width: 100%;
-  height: 100%;
-  background: #291a5e;
-  position: relative;
-
-  .form-wrap {
-    width: 370px;
-    height: 300px;
+  height: 100vh;
+  // background-color: #f0f2f5;
+  .login-content {
+    width: 328px;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    border-radius: 10px;
-    background: linear-gradient(to bottom, rgb(164, 60, 246), rgb(0, 184, 249));
-    padding: 2px;
-
-    :deep(.el-form) {
-      width: 100%;
-      height: 100%;
-      background: linear-gradient(to bottom, rgb(30, 22, 75), rgb(43, 26, 97));
-      border-radius: 10px;
-
-      h1 {
-        font-size: 24px;
-        text-align: center;
-        color: #fff;
+    ::v-deep(.ant-tabs-nav)::before {
+      border: 1px solid #e2e4e7;
+    }
+    .header {
+      .logo {
+        width: 44px;
+        height: 44px;
       }
-
-      .submit-btn {
-        width: 250px;
-        margin-left: 90px;
-      }
-
-      .el-input {
-        width: 250px;
+      span {
+        font-size: 33px;
+        font-weight: 600;
       }
     }
+    ::v-deep(.el-input) {
+      height: 40px;
+    }
   }
+}
+:deep(.el-tabs__nav-scroll) {
+  display: flex;
+  justify-content: center;
 }
 </style>
